a39c7d1102857858aa9e4edec4f3700868974b8a
Alex Mejicanos <alexmejicanos@outlook.com>
2019-08-05 07:44:25 -0600

    Add all migrations

---
+++ migrations/20190803045406-create-customers.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      company: {
+        type: Sequelize.BOOLEAN
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      levelsId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Levels',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Customers');
+  }
+};
